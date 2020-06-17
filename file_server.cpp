80f596582adf6652c53604b0f072ecdb3b1c8566
Ilya Popovnin <itdbnice@gmail.com>
2020-06-17 19:38:43 +0300

    create definitions

--- file_server.cpp
+++ file_server.cpp
@@ -1,5 +1,25 @@
-//
-// Created by Ilya Popovnin on 2020-06-17.
-//
-
 #include "file_server.h"
+
+void FileServer::start() {
+
+}
+
+void FileServer::stop() {
+
+}
+
+void FileServer::run() {
+
+}
+
+void FileServer::startWritingFile(std::string name) {
+
+}
+
+void FileServer::writeBlock(std::string name, uint8_t *block, int size) {
+
+}
+
+void FileServer::finishWritingFile(std::string name) {
+
+}
