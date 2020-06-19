e4be0186f459c61337ff2f424a7d374303e56c2b
AnimeMonster <stenli97@gmail.com>
2020-06-20 02:00:35 +0300

    fix up

--- client-py/client-py.py
+++ client-py/client-py.py
@@ -4,27 +4,37 @@ from sys import argv
 
 
 def send_data_block(buffer, file_name, first_block):
+    if first_block:
+        sock.send(file_name.encode('ascii', 'replace'))
+        sock.send(b'\0')
     length = (len(buffer)).to_bytes(4, 'big')
     sock.send(length)
-    if first_block:
-        sock.send(file_name.encode("utf-8"))
-        sock.send(b'\0')
-    sock.send(buffer)
+    sock.send(b''.join(buffer))
 
 
+if len(argv) != 4:
+    print("Insufficient data entered.\n"
+          "Check for the following parameters:\n"
+          "script name\n"
+          "address\n"
+          "local name\n"
+          "remote name\n")
 script, address, local_name, remote_name = argv
+
 sock = socket.socket()
 sock.connect((address, 5678))
-print("адрес:", address, "loc_name:", local_name, "rem_name:", remote_name)
+
 with open(local_name, "rb") as local_file:
     buffer = []
     first_block = True
-    while byte := local_file.read(1):
+    byte = local_file.read(1)
+    while byte:
         buffer.append(byte)
         if len(buffer) == 1024:
             send_data_block(buffer, remote_name, first_block)
             first_block = False
             buffer = []
+        byte = local_file.read(1)
     if len(buffer) > 0:
         send_data_block(buffer, remote_name, first_block)
         first_block = False
